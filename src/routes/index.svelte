a2f69cba0664650663997d5fb24cf86a93b8344d
Robert Reinert <robert.reinert@gmail.com>
2022-02-14 13:23:37 -0800

    Headless UI added

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,20 @@
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+  import { Popover, PopoverPanel, PopoverButton } from "@rgossiaux/svelte-headlessui";
+</script>
+
+
+
+  <Popover className="relative">
+    <PopoverButton>Solutions</PopoverButton>
+
+    <PopoverPanel class="absolute z-10">
+      <div class="grid grid-cols-2">
+        <a href="/analytics">Analytics</a>
+        <a href="/engagement">Engagement</a>
+        <a href="/security">Security</a>
+        <a href="/integrations">Integrations</a>
+      </div>
+
+      <img src="/solutions.jpg" alt="" />
+    </PopoverPanel>
+  </Popover>
